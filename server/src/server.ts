f5daa0d1c68eea0fb8a525b6bedc9896fd3a3414
vitor <vitorduarte.vds7@gmail.com>
2022-10-19 15:59:09 -0300

    server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,27 @@
 import express from "express";
 
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 const app = express();
 
-app.get("/ads", (req, res) => {
-    res.json([
-        { id: 1, name: "Anuncio" },
-        { id: 2, name: "Anuncio" },
-        { id: 3, name: "Anuncio" },
-    ]);
+app.get("/games", async (req, res) => {
+    const games = await prisma.game.findMany();
+
+    return res.json(games);
+});
+app.post("/ads", (req, res) => {
+    return res.status(201).json([]);
+});
+app.get("/games/:id/ads", (req, res) => {
+    const gameId = req.params.id;
+
+    return res.send(gameId);
+});
+
+app.get("/ads/:id/discord", (req, res) => {
+    // const adId = req.params.id;
+    res.json([]);
 });
 
 app.listen(3000, () => {
