eaa9ef9b2b2713c7f337cb10218f61c5f95d288d
vitor <vitorduarte.vds7@gmail.com>
2022-11-04 18:07:19 -0300

    modal

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,30 @@
-import { MagnifyingGlassPlus } from "phosphor-react";
 import "./styles/main.css";
 
 import logoImg from "./assets/LogoeSports.svg";
+import { GamerBanner } from "./components/GameBanner";
+import { CreateAdBanner } from "./components/CreateAdBanner";
+import { useEffect, useState } from "react";
+import * as Dialog from "@radix-ui/react-dialog";
+
+interface Game {
+    id: string;
+    title: string;
+    bannerUrl: string;
+    _count: {
+        ads: number;
+    };
+}
 
 function App() {
+    const [games, setGames] = useState<Game[]>([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/games")
+            .then((resp) => resp.json())
+            .then((data) => {
+                setGames(data);
+            });
+    }, []);
     return (
         <div className=" max-w-[1344px] mx-auto flex flex-col items-center my-20 ">
             <img src={logoImg} alt="" />
@@ -16,87 +37,98 @@ function App() {
                 is here.
             </h1>
             <div className="grid grid-cols-6 gap-6 mt-16">
-                <a className="relative rounded-lg overflow-hidden" href="">
-                    <img src="/game1.png" alt="" />
-                    <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-                        <strong className="text-white font-bold">
-                            League of Legends
-                        </strong>
-                        <span className="text-zinc-400 text-sm block">
-                            4 announcements
-                        </span>
-                    </div>
-                </a>
-                <a className="relative rounded-lg overflow-hidden" href="">
-                    <img src="/game2.png" alt="" />
-                    <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-                        <strong className="text-white font-bold">DOTA 2</strong>
-                        <span className="text-zinc-400 text-sm block">
-                            4 announcements
-                        </span>
-                    </div>
-                </a>
-                <a className="relative rounded-lg overflow-hidden" href="">
-                    <img src="/game3.png" alt="" />
-                    <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-                        <strong className="text-white font-bold">
-                            Counter Strike
-                        </strong>
-                        <span className="text-zinc-400 text-sm block">
-                            4 announcements
-                        </span>
-                    </div>
-                </a>
-                <a className="relative rounded-lg overflow-hidden" href="">
-                    <img src="/game4.png" alt="" />
-                    <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-                        <strong className="text-white font-bold">
-                            Apex Legends
-                        </strong>
-                        <span className="text-zinc-400 text-sm block">
-                            4 announcements
-                        </span>
-                    </div>
-                </a>
-                <a className="relative rounded-lg overflow-hidden" href="">
-                    <img src="/game5.png" alt="" />
-                    <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-                        <strong className="text-white font-bold">
-                            Fortnite
-                        </strong>
-                        <span className="text-zinc-400 text-sm block">
-                            4 announcements
-                        </span>
-                    </div>
-                </a>
-                <a className="relative rounded-lg overflow-hidden" href="">
-                    <img src="/game6.png" alt="" />
-                    <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-                        <strong className="text-white font-bold">
-                            World of WarCraft
-                        </strong>
-                        <span className="text-zinc-400 text-sm block">
-                            4 announcements
-                        </span>
-                    </div>
-                </a>
+                {games.map((game) => {
+                    return (
+                        <GamerBanner
+                            key={game.id}
+                            bannerUrl={game.bannerUrl}
+                            title={game.title}
+                            adsCount={game._count.ads}
+                        />
+                    );
+                })}
             </div>
-            <div className="pt-1 bg-nlw-gradient self-stretch  rounded-lg mt-8">
-                <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
-                    <div>
-                        <strong className="text-2xl text-white font-black block">
-                            Couldn't find your duo?
-                        </strong>
-                        <span className="text-zinc-400">
-                            Post an announcement to find new players{" "}
-                        </span>
-                    </div>
-                    <button className="py-3 px-4 bg-violet-500 hover:bg-violet-700 text-white rounded flex items-center gap-3">
-                        <MagnifyingGlassPlus size={24} />
-                        Post announcement
-                    </button>
-                </div>
-            </div>
+            <Dialog.Root>
+                <CreateAdBanner />
+                <Dialog.Portal>
+                    <Dialog.Overlay className="bg-black/60 inset-0 fixed " />
+                    <Dialog.Content className="fixed bg-[#2a2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25">
+                        <Dialog.Title className="text-3xl text-white font-black">
+                            Publish a announcement
+                        </Dialog.Title>
+                        <Dialog.Content>
+                            <Dialog.Content>
+                                <form>
+                                    <div>
+                                        <label htmlFor="game">What game?</label>
+                                        <input
+                                            type="text"
+                                            id="game"
+                                            placeholder="Choose your game"
+                                        />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="name">
+                                            Your Nickname
+                                        </label>
+                                        <input
+                                            type="text"
+                                            id="name"
+                                            placeholder="Nickname"
+                                        />
+                                    </div>
+                                    <div>
+                                        <div>
+                                            <label htmlFor="yearsPlaying">
+                                                How many years have you been
+                                                playing?
+                                            </label>
+                                            <input
+                                                type="number"
+                                                id="yearsPlaying"
+                                            />
+                                        </div>
+                                        <div>
+                                            <label htmlFor="discord">
+                                                What's your discord?
+                                            </label>
+                                            <input
+                                                type="text"
+                                                placeholder="user#0000"
+                                                id="discord"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div>
+                                            <label htmlFor="weekDays">
+                                                When do you play?
+                                            </label>
+                                        </div>
+                                        <div>
+                                            <label htmlFor="hourStart">
+                                                At which hour?
+                                            </label>
+                                            <div>
+                                                <input
+                                                    type="time"
+                                                    placeholder="from"
+                                                    id="hourStart"
+                                                />
+                                                <input
+                                                    type="time"
+                                                    placeholder="To"
+                                                    id="hourEnd"
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </Dialog.Content>
+                        </Dialog.Content>
+                    </Dialog.Content>
+                </Dialog.Portal>
+            </Dialog.Root>
         </div>
     );
 }
