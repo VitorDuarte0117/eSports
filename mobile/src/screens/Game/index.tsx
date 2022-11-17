b4aadf6bc04eac7210170600555d12d07211e5de
vitor <vitorduarte.vds7@gmail.com>
2022-11-17 15:06:08 -0300

    mobile

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,7 +1,42 @@
-import { SafeAreaView } from "react-native";
+import { Image, SafeAreaView, TouchableOpacity, View } from "react-native";
+import { useRoute, useNavigation } from "@react-navigation/native";
+import { Background } from "../../components/Background";
+import { Entypo } from "@expo/vector-icons";
+import logoImg from "../../assets/logo-nlw-esports.png";
 
 import { styles } from "./styles";
+import { GameParams } from "../../@types/navigation";
+import { THEME } from "../../theme";
+import { Heading } from "../../components/Heading";
 
 export function Game() {
-    return <SafeAreaView style={styles.container}></SafeAreaView>;
+    const navigation = useNavigation();
+    const route = useRoute();
+    const game = route.params as GameParams;
+
+    function handleGoBack() {
+        navigation.goBack();
+    }
+    return (
+        <Background>
+            <SafeAreaView style={styles.container}>
+                <View style={styles.header}>
+                    <TouchableOpacity onPress={handleGoBack}>
+                        <Entypo
+                            name="chevron-thin-left"
+                            color={THEME.COLORS.CAPTION_300}
+                            size={20}
+                        />
+                    </TouchableOpacity>
+
+                    <Image source={logoImg} style={styles.logo} />
+                    <View style={styles.right} />
+                </View>
+                <Heading
+                    title={game.title}
+                    subtitle="Log in and start to play!"
+                />
+            </SafeAreaView>
+        </Background>
+    );
 }
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { Image, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
 
 import logoImg from "../../assets/logo-nlw-esports.png";
+import { Background } from "../../components/Background";
 
 import { GameCard, GameCardProps } from "../../components/GameCard";
 import { Heading } from "../../components/Heading";
@@ -11,26 +13,38 @@ import { styles } from "./styles";
 
 export function Home() {
     const [games, setGames] = useState<GameCardProps[]>([]);
+    const navigation = useNavigation();
+
+    function handleOpenGame({ id, title, bannerUrl }: GameCardProps) {
+        navigation.navigate("game", { id, title, bannerUrl });
+    }
     useEffect(() => {
         fetch("http://192.168.15.7:3000/games").then((res) =>
             res.json().then((data) => setGames(data))
         );
     }, []);
     return (
-        <SafeAreaView style={styles.container}>
-            <Image source={logoImg} style={styles.logo} />
-            <Heading
-                title="Find your Duo"
-                subtitle="Select the game you want to play"
-            />
-            <FlatList
-                data={games}
-                keyExtractor={(item) => item.id}
-                renderItem={({ item }) => <GameCard data={item} />}
-                showsHorizontalScrollIndicator={false}
-                horizontal
-                contentContainerStyle={styles.contentList}
-            />
-        </SafeAreaView>
+        <Background>
+            <SafeAreaView style={styles.container}>
+                <Image source={logoImg} style={styles.logo} />
+                <Heading
+                    title="Find your Duo"
+                    subtitle="Select the game you want to play"
+                />
+                <FlatList
+                    data={games}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({ item }) => (
+                        <GameCard
+                            data={item}
+                            onPress={() => handleOpenGame(item)}
+                        />
+                    )}
+                    showsHorizontalScrollIndicator={false}
+                    horizontal
+                    contentContainerStyle={styles.contentList}
+                />
+            </SafeAreaView>
+        </Background>
     );
 }
